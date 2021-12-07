fbddc8d6621b64a132ec13c36b169bb6d2776ad7
karnena <santoshkarnena95811@gmail.com>
2021-12-07 16:40:56 +0530

    login updated

--- sql_app/mymodules/models.py
+++ sql_app/mymodules/models.py
@@ -1,26 +1,16 @@
+from typing import Text
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import TEXT
 
-from .database import Base
+from library.database import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "users_details"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    # is_active = Column(Boolean, default=True)
+    user_name = Column(TEXT, unique=True, index=True)
+    email = Column(TEXT, unique=True, index=True)
+    password = Column(TEXT)
 
-    items = relationship("Item", back_populates="owner")
-
-
-class Item(Base):
-    __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="items")
--- sql_app/mymodules/schemas.py
+++ sql_app/mymodules/schemas.py
@@ -1,37 +1,19 @@
-from typing import List, Optional
+from typing import List, Optional, Text
 
 from pydantic import BaseModel
+from sqlalchemy.sql.sqltypes import TEXT,INTEGER
 
-
-class ItemBase(BaseModel):
-    title: str
-    description: Optional[str] = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
 
 
 class UserBase(BaseModel):
     email: str
 
 
-class UserCreate(UserBase):
-    password: str
-
-
 class User(UserBase):
     id: int
-    # is_active: bool
-    items: List[Item] = []
-
+    user_name: str
+    email: str
+    password: str
+    
     class Config:
         orm_mode = True
